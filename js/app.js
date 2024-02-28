4ef826cbbc48595ad30de0bf7129aa503a86ac15
Jimmy Mayta <jimmyymaytaj@gmail.com>
2024-02-28 11:43:03 -0400

    juego 2

---
+++ js/app.js
@@ -0,0 +1,6 @@
+fetch("../data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.error("Error loading JSON file", error));
